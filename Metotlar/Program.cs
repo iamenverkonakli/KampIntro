a3c379c46e9418a4bde796efed67065dcc5e6bd3
konak <konak@LAPTOP-L146SODG>
2022-07-21 17:39:32 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyatı = 14;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyatı = 80;
+            urun2.Aciklama = "Dyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);   
+                Console.WriteLine(urun.Fiyatı);   
+                Console.WriteLine(urun.Aciklama);   
+                Console.WriteLine("----------------");    
+            }
+            Console.WriteLine("--------Metotlar-----------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12,10);
+        }
+    }
+}
