e60dcb844c7d1dbad7cf05da3d19f2e4c058f056
konak <konak@LAPTOP-L146SODG>
2022-07-21 17:39:32 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " "+ customer.Surname+ " " + customer.AccountNumber +" eklendi."  );
+        }
+        public void CustomerList(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " " + customer.AccountNumber + " listelendi.");
+
+        }
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " " + customer.AccountNumber + " silindi.");
+
+        }
+
+
+    }
+}
---
+++ kamp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace kamp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Kurs kurslar1 = new Kurs();
+            kurslar1.KursAdi = "java";
+            kurslar1.Egıtmen = "Enver";
+            kurslar1.IzlenmeOrani = 70;
+
+            Kurs kurslar2 = new Kurs();
+            kurslar2.KursAdi = "R";
+            kurslar2.Egıtmen = "eren";
+            kurslar2.IzlenmeOrani = 100;
+
+            Kurs kurslar3 = new Kurs();
+            kurslar3.KursAdi = "pyhton";
+            kurslar3.Egıtmen = "sena";
+            kurslar3.IzlenmeOrani = 86;
+
+            Kurs[] kurslar = new Kurs[] {kurslar1, kurslar2, kurslar3  };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + "=" + kurs.Egıtmen);
+            }
+        }
+        class Kurs
+        {
+            public string KursAdi { get; set; }
+            public string Egıtmen { get; set; }
+            public int IzlenmeOrani { get; set; }
+        }
+    }
+}
