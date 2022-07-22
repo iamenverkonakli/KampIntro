65c352c6299e90ec5a6ddd97cedc0892e0e6bf5a
iamenverkonakli <konaklienver@gmail.com>
2022-07-22 15:21:44 +0300

    22.07.2022

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,22 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-             
+             Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitPrice = 3;
+
+            Product product2= new Product() {Id=2,CategoryId=5,UnitsInStock=5, UnitPrice=35,ProductName="Kalem" };
+
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+
         }
     }
 }
